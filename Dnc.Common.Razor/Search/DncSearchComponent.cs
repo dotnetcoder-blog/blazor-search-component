ef8804c663785db116d127330b9f87a899ef44ff
Alaaddin <alaaiddin.albakkour@gmail.com>
2024-08-24 19:43:44 +0200

    Add project files.

---
+++ Dnc.Common.Razor/Search/DncSearchComponent.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dnc.Common.Razor.Search
+{
+    public class DncSearchComponent<TItem> : ComponentBase
+    {
+        [Parameter]
+        public EventCallback<string> OnSearch { get; set; }
+
+        [Parameter]
+        public string PlaceholderText { get; set; } = "Search...";
+
+        [Parameter]
+        public string ButtonText { get; set; } = "Search";
+
+        [Parameter]
+        public string CssButton { get; set; } = "btn-primary";
+
+        protected string searchTerm;
+        protected async Task HandleSearch()
+        {
+            await OnSearch.InvokeAsync(searchTerm);
+        }
+
+        protected async Task HandleEnter(KeyboardEventArgs e)
+        {
+            if (e.Key == "Enter")
+            {
+                await OnSearch.InvokeAsync(searchTerm);
+            }
+        }
+    }
+}
---
+++ Dnc.Search.WebApp/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Dnc.Search.WebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+    }
+}
